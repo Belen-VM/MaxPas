61578621356a6132020655755391152e839c56d6
lahnie <bvargasmoran@gmail.com>
2024-06-23 00:30:49 -0400

    Cargas monto listo!!

---
+++ cajas.js
@@ -0,0 +1,46 @@
+const cajas=[
+    {
+    numero: 1,
+    nombre: '#Caja1',
+    tarjetas: [
+        {
+            numero: 1,
+            fecha:'',
+            numeroChip: 1,
+            saldoAnterior: 0,
+            montoCarga: 0,
+            estado:0,
+        },
+        {
+            numero: 2,
+            fecha:'',
+            numeroChip: 2,
+            saldoAnterior: 0,
+            montoCarga: 0,
+            estado:0,
+        }
+    ]
+}, 
+{
+    numero: 2,
+    nombre: '#Caja2',
+    tarjetas: [
+        {
+            numero: 3,
+            fecha:'',
+            numeroChip: 3,
+            saldoAnterior: 0,
+            montoCarga: 500,
+            estado:0,
+        },
+        {
+            numero: 4,
+            fecha:'',
+            numeroChip: 4,
+            saldoAnterior: 0,
+            montoCarga: 240,
+            estado:0,
+        }
+    ]
+}
+]
---
+++ tarjetas.js
@@ -0,0 +1,8 @@
+const tarjetas=[{
+    numero: 1,
+    fecha:'',
+    numeroChip: 1,
+    saldoAnterior: 0,
+    montoCarga: 0,
+    estado:1,
+}]
